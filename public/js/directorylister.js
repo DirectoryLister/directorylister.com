1a3622a1b2c327d433913638d1e7ed476ea94858
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-01-11 16:02:52 -0700

    Moved parts of code into their own partials. Additional style tweaks and refintements.

--- public/js/directorylister.js
+++ public/js/directorylister.js
@@ -2,39 +2,5 @@ $(document).ready(function() {
     
     // Enable Tooltips
     $('[rel="tooltip"]').tooltip();
-    
-    
-    // Enable scrollspy on navigation
-    $('#site-navigation').scrollspy();
-    
-    
-    // // Get top of navigation
-    // var originalTop = $('#site-navigation').offset().top;
-//     
-    // // Run nav position check on page load
-    // checkNavPosition(originalTop);
-//     
-    // // Run nav position check on scoll 
-    // $(window).scroll(function() {
-        // checkNavPosition(originalTop);
-    // })
 
-    
-    // Animate page scroll when returning to the top
-    $('#page-top-link').click(function() {
-       $('html, body').animate({ scrollTop: 0 }, 'fast');
-       return false; 
-    });
-    
 });
-
-
-function checkNavPosition(elTop) {
-    if ($(window).scrollTop() >= elTop) {
-        $('body').addClass('scrolled');
-        $('#page-top-link').show();
-    } else {
-        $('body').removeClass('scrolled');
-        $('#page-top-link').hide();
-    }
-}
