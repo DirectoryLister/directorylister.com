8d6b9335e6f82fb484a04c2aecc198ab2e6cb7a3
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-01-22 08:46:06 -0700

    Added top of page link button

--- public/js/directorylister.js
+++ public/js/directorylister.js
@@ -2,5 +2,31 @@ $(document).ready(function() {
     
     // Enable Tooltips
     $('[rel="tooltip"]').tooltip();
+    
+    
+    // Show/hide top button on page load
+    checkTopButtonVisibility('#top-button', '#intro');
+    
+    // Show/hide top button on page scroll
+    $(window).scroll(function() {
+        checkTopButtonVisibility('#top-button', '#intro');
+    });
 
 });
+
+
+/**
+ * Determines wether or not the Top of Page button should be shown or not
+ * based on how far down the page is scrolled.
+ * 
+ * @author Chris Kankiewicz (http://www.ChrisKankiewicz.com)
+ */
+function checkTopButtonVisibility(buttonEl, scrollEl) {
+    
+    if($(window).scrollTop() >= $(scrollEl).offset().top) {
+        $(buttonEl).show();
+    } else {
+        $(buttonEl).hide();
+    }
+    
+}
