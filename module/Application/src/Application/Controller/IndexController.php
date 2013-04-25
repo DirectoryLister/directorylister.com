a5b05d5526ab8d6ea10952ec93d8ce8da17a106c
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-04-24 22:35:30 -0700

    Changed GitHub API requests to use Curl and pass along a useragent string See: http://developer.github.com/changes/2013-04-24-user-agent-required/

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -15,25 +15,25 @@ use Zend\Cache\StorageFactory;
 
 class IndexController extends AbstractActionController
 {
-    
+
     public function indexAction()
     {
-        
+
         // Instantiate the view object
         $view = new ViewModel();
-        
+
         // Get config object
         $config = $this->getServiceLocator()->get('config');
-        
-        
+
+
         // Pass Stripe key to the view
         if ($_SERVER['HTTP_HOST'] == 'directorylister.com') {
             $view->stripeKey = $config['stripe']['live']['publishable_key'];
         } else {
             $view->stripeKey = $config['stripe']['test']['publishable_key'];
         }
-        
-        
+
+
         // Instantiate the cache object
         $cache = StorageFactory::factory(array(
             'adapter' => array(
@@ -47,73 +47,106 @@ class IndexController extends AbstractActionController
                 'exception_handler' => array('throw_exceptions' => false),
             )
         ));
-        
-        
+
+
         // Attempt to fetch stargazers from the cache
         $view->stargazers = $cache->getItem('stargazers', $success);
-        
+
         if (!$success) {
-            
+
+            // Initialize curl
+            $curl = curl_init();
+
+            // Set curl options
+            curl_setopt($curl, CURLOPT_URL, 'https://api.github.com/repos/DirectoryLister/DirectoryLister/stargazers');
+            curl_setopt($curl, CURLOPT_USERAGENT, 'http://www.directorylister.com');
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
             // Fetch stargazers from GitHub
-            $apiResults = file_get_contents('https://api.github.com/repos/DirectoryLister/DirectoryLister/stargazers');
+            $apiResults = curl_exec($curl);
             $dataObject = json_decode($apiResults);
-            
+
             // Pass stargazer count to the view
             $view->stargazers = count($dataObject);
-            
+
             // Cache the data
             $cache->setItem('stargazers', $view->stargazers);
-            
+
+            // Close curl handle
+            curl_close($curl);
+
         }
-        
+
         // print_r($view->stargazers); die(); // Debugging
-        
-        
+
+
         // Attempt to fetch forks from the cache
         $view->forks = $cache->getItem('forks', $success);
-        
+
         if (!$success) {
-            
+
+            // Initialize curl
+            $curl = curl_init();
+
+            // Set curl options
+            curl_setopt($curl, CURLOPT_URL, 'https://api.github.com/repos/DirectoryLister/DirectoryLister/forks');
+            curl_setopt($curl, CURLOPT_USERAGENT, 'http://www.directorylister.com');
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
             // Fetch forks from GitHub
-            $apiResults = file_get_contents('https://api.github.com/repos/DirectoryLister/DirectoryLister/forks');
+            $apiResults = curl_exec($curl);
             $dataObject = json_decode($apiResults);
-            
+
             // Pass fork count to the view
             $view->forks = count($dataObject);
-            
+
             // Cache the data
             $cache->setItem('forks', $view->forks);
-            
+
+            // Close curl handle
+            curl_close($curl);
+
         }
 
         // print_r($view->forks); die(); // Debugging
-        
-        
+
+
         // Attempt to fetch forks from the cache
         $view->tags = $cache->getItem('tags', $success);
-        
+
         if (!$success) {
-            
+
+            // Initialize curl
+            $curl = curl_init();
+
+            // Set curl options
+            curl_setopt($curl, CURLOPT_URL, 'https://api.github.com/repos/DirectoryLister/DirectoryLister/tags');
+            curl_setopt($curl, CURLOPT_USERAGENT, 'http://www.directorylister.com');
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
             // Fetch tags from GitHub
-            $apiResults = file_get_contents('https://api.github.com/repos/DirectoryLister/DirectoryLister/tags');
-            
+            $apiResults = curl_exec($curl);
+
             // Pass tags to the view
             $view->tags = json_decode($apiResults);
-            
+
             // Cache the data
             $cache->setItem('tags', $view->tags);
-            
+
+            // Close curl handle
+            curl_close($curl);
+
         }
-        
+
         // print_r($view->tags); die(); // Debugging
-        
-        
+
+
         // Latest download links
         $view->name  = $view->tags[0]->name;
         $view->dlZip = $view->tags[0]->zipball_url;
         $view->dlTar = $view->tags[0]->tarball_url;
-        
-        
+
+
         // Return the view
         return $view;
     }
