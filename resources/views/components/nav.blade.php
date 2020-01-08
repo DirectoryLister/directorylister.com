d1a20cac07f3da5f6e758ea9365dac9f3f2dd17b
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-08 09:00:29 -0700

    Extracted some sections to dedicated components

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,28 @@
+<nav class="bg-white">
+    <div class="container max-w-4xl mx-auto py-6 px-4 flex justify-between items-center lg:px-0">
+        <div class="flex items-center">
+            <img src="{{ asset('images/directory-lister.svg') }}" class="w-12 h-12 mr-4" alt="Directory Lister Logo">
+
+            <h1 class="font-serif text-3xl">
+                Directory Lister
+            </h1>
+        </div>
+
+        <div class="flex items-center">
+            <a href="#" class="text-gray-600 hover:text-gray-800 hover:underline">
+                v3.0.0
+            </a>
+
+            <a href="#" title="GitHub" class="text-gray-600 ml-4 hover:text-gray-800">
+                <i class="fab fa-github fa-lg"></i>
+            </a>
+
+            <a href="#" title="Twitter" class="text-gray-600 ml-4 hover:text-gray-800">
+                <i class="fab fa-twitter fa-lg"></i>
+            </a>
+
+            <a href="#" title="Slack" class="text-gray-600 ml-4 hover:text-gray-800">
+                <i class="fab fa-slack fa-lg"></i>
+            </a>
+        </div>
+</nav>
