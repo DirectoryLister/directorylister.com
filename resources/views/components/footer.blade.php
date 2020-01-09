66a87b1e1ecd2ca0047013a5862dfae208a87e34
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-09 14:45:29 -0700

    Added real hrefs to all anchor tags

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="bg-white">
     <div class="container max-w-4xl mx-auto px-4 py-12 text-center text-gray-600 lg:px-0">
         <p class="my-4">
-            Released under the <a href="#" class="underline hover:text-gray-800">MIT license</a>
-            &bull; <a href="#" class="underline hover:text-gray-800">View source</a>
+            Released under the <a href="https://github.com/DirectoryLister/DirectoryLister/blob/master/LICENSE" class="underline hover:text-gray-800">MIT license</a>
+            &bull; <a href="https://github.com/DirectoryLister/DirectoryLister" class="underline hover:text-gray-800">View source</a>
         </p>
 
         <p class="my-4">
