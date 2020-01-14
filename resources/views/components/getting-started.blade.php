e5f6421fe09ca743bc0911129c39a242b747cf9a
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-13 22:56:15 -0700

    Fixed some incorrect links

--- resources/views/components/getting-started.blade.php
+++ resources/views/components/getting-started.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-blue-900 text-white">
     <div class="container max-w-4xl mx-auto px-4 py-12 text-center lg:px-0">
-        <a href="https://github.com/PHLAK/SemVer/releases" class="inline-block rounded-lg bg-blue-600 shadow px-6 py-4 text-lg hover:bg-blue-700 hover:shadow-md">
+        <a href="https://github.com/DirectoryLister/DirectoryLister/releases" class="inline-block rounded-lg bg-blue-600 shadow px-6 py-4 text-lg hover:bg-blue-700 hover:shadow-md">
             Get Directory Lister
         </a>
 
