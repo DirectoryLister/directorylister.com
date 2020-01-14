c19a83a52f252bb90306782e3a7be7d8c7531dc0
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-14 13:22:18 -0700

    Mobile layout improvements

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,15 +1,15 @@
 <nav class="bg-white">
-    <div class="container max-w-4xl mx-auto py-6 px-4 flex justify-between items-center lg:px-0">
+    <div class="container max-w-4xl mx-auto py-6 px-4 flex flex-col justify-between items-center sm:flex-row lg:px-0">
         <div class="flex items-center">
             <img src="{{ asset('images/directory-lister.svg') }}" class="w-12 h-12 mr-4" alt="Directory Lister Logo">
 
-            <h1 class="font-serif text-2xl sm:text-3xl">
+            <h1 class="font-serif text-3xl">
                 Directory Lister
             </h1>
         </div>
 
-        <div class="flex items-center">
-            <a href="https://github.com/DirectoryLister/DirectoryLister/releases" class="text-gray-600 hover:text-gray-800 hidden hover:underline sm:inline-block">
+        <div class="flex items-center pt-4 sm:pt-0">
+            <a href="https://github.com/DirectoryLister/DirectoryLister/releases" class="text-gray-600 hover:text-gray-800 hover:underline inline-block">
                 v3.0.0-beta
             </a>
 
@@ -23,6 +23,10 @@
 
             <a href="https://ln.phlak.net/join-slack" title="Slack" class="text-gray-600 ml-4 hover:text-gray-800">
                 <i class="fab fa-slack fa-lg"></i>
+            </a>
+
+            <a href="https://spectrum.chat/directory-lister" title="Spectrum" class="text-gray-600 ml-4 hover:text-gray-800">
+                <i class="fas fa-comments fa-lg"></i>
             </a>
         </div>
 </nav>
--- resources/views/components/sponsor.blade.php
+++ resources/views/components/sponsor.blade.php
@@ -1,12 +1,16 @@
 <div class="bg-gray-800 text-white">
-    <div class="container max-w-4xl mx-auto py-12 px-4 flex flex-col justify-between items-center lg:px-0">
+    <div class="container max-w-4xl mx-auto py-12 px-4 flex flex-col justify-between items-center text-center lg:px-0">
             <div>
-                <a href="https://github.com/sponsors/PHLAK" class="inline-block rounded-lg bg-pink-600 shadow px-6 py-4 mx-2 text-lg text-white hover:bg-pink-700 hover:shadow-md">
-                    <i class="fas fa-heart fa-lg fa-fw"></i> Become a Sponsor
+                <a href="https://github.com/sponsors/PHLAK" class="inline-block rounded-lg bg-pink-600 shadow p-4 mx-2 text-lg text-white sm:px-6 hover:bg-pink-700 hover:shadow-md">
+                    <i class="fas fa-heart fa-lg fa-fw"></i>
+                    <span class="hidden sm:inline">Become a Sponsor</span>
+                    <span class="inline sm:hidden">Sponsor</span>
                 </a>
 
-                <a href="https://www.paypal.me/ChrisKankiewicz" class="inline-block rounded-lg bg-green-600 shadow px-6 py-4 mx-2 text-lg text-white hover:bg-green-700 hover:shadow-md">
-                    <i class="fas fa-donate fa-lg fa-fw "></i> Make a Donation
+                <a href="https://www.paypal.me/ChrisKankiewicz" class="inline-block rounded-lg bg-green-600 shadow p-4 m-2 text-lg text-white sm:my-0 sm:px-6 hover:bg-green-700 hover:shadow-md">
+                    <i class="fas fa-donate fa-lg fa-fw "></i>
+                    <span class="hidden sm:inline">Make a Donation</span>
+                    <span class="inline sm:hidden">Donate</span>
                 </a>
             </div>
 
