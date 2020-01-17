cf842ec920fa9f9e8baae9a2abf406ec863d668f
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-17 14:11:46 -0700

    Fetch release info from GitHub API

---
+++ app/GitHub/Client.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\GitHub;
+
+use GuzzleHttp\Client as GuzzleHttpClient;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Facades\Log;
+
+class Client
+{
+    /** @var GuzzleHttpClient The HTTP client */
+    protected $client;
+
+    /**
+     * Create a new GitHub Client instance.
+     *
+     * @param string $authToken GitHub OAuth token
+     * @param array  $config    GuzzleHttp Client config
+     */
+    public function __construct(string $authToken, array $config = [])
+    {
+        $this->client = new GuzzleHttpClient(array_replace_recursive([
+            'base_uri' => config('services.github.base_uri'),
+            'connect_timeout' => 5,
+            'headers' => [
+                'Authorization' => "Token {$authToken}",
+            ],
+            'timeout' => 30,
+        ], $config));
+    }
+
+    /**
+     * Get the latest release for a repository.
+     *
+     * @param string $owner The repository owner
+     * @param string $repo  The repository name
+     *
+     * @throws JsonException
+     *
+     * @return object
+     */
+    public function latestRelease(string $owner, string $repo): object
+    {
+        try {
+            $response = $this->client->get("repos/{$owner}/{$repo}/releases/latest");
+        } catch (ClientException $exception) {
+            Log::error($exception);
+
+            return json_decode('{}');
+        }
+
+        return json_decode((string) $response->getBody(), false, 512, JSON_THROW_ON_ERROR);
+    }
+
+    /**
+     * Get the latest release for a repository.
+     *
+     * @param string $owner The repository owner
+     * @param string $repo  The repository name
+     *
+     * @throws JsonException
+     *
+     * @return object
+     */
+    public function latestPrerelease(string $owner, string $repo): object
+    {
+        try {
+            $response = $this->client->get("repos/{$owner}/{$repo}/releases");
+        } catch (ClientException $exception) {
+            Log::error($exception);
+
+            return json_decode('{}');
+        }
+
+        return json_decode((string) $response->getBody(), false, 512, JSON_THROW_ON_ERROR)[0];
+    }
+}
--- config/services.php
+++ config/services.php
@@ -30,4 +30,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'github' => [
+        'base_uri' => env('GITHUB_BASE_URI', 'https://api.github.com/'),
+        'token' => env('GITHUB_TOKEN'),
+    ],
+
 ];
