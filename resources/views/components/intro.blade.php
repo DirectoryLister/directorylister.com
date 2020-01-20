0071fe7dec22bc8b7b969c299ab12010dd25d65c
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-20 10:57:27 -0700

    Updated intro text

--- resources/views/components/intro.blade.php
+++ resources/views/components/intro.blade.php
@@ -2,10 +2,9 @@
     <div class="container max-w-4xl mx-auto px-4 py-12 lg:px-0">
         <p class="font-serif leading-loose text-lg mb-12 sm:text-xl">
             <span class="font-bold">Directory Lister</span> is the easiest way
-            to share the contents of any web-accessible folder and navigate
-            there within. With a zero configuration, drag-and-drop
-            installation you'll be up and running in
-            <span class="underline">less than a minute</span>.
+            to expose the contents of any web-accessable folder for browsing
+            and sharing. With a zero configuration, drag-and-drop installation
+            you'll be up and running in <span class="underline">less than a minute</span>.
         </p>
 
         <div id="beer-slider" class="beer-slider rounded-lg shadow-lg mb-6" data-beer-label="Dark">
