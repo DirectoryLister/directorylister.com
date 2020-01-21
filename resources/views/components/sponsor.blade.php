0d13bacf045533961effb6f535ac5b260b7ad2b9
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-20 22:50:25 -0700

    Removed extra closing div

--- resources/views/components/sponsor.blade.php
+++ resources/views/components/sponsor.blade.php
@@ -1,22 +1,21 @@
 <div class="bg-gray-800 text-white">
     <div class="container max-w-4xl mx-auto py-12 px-4 flex flex-col justify-between items-center text-center lg:px-0">
-            <div>
-                <a href="https://github.com/sponsors/PHLAK" class="inline-block rounded-lg bg-pink-600 shadow p-4 mx-2 text-lg text-white sm:px-6 hover:bg-pink-700 hover:shadow-md">
-                    <i class="fas fa-heart fa-lg fa-fw"></i>
-                    <span class="hidden sm:inline">Become a Sponsor</span>
-                    <span class="inline sm:hidden">Sponsor</span>
-                </a>
+        <div>
+            <a href="https://github.com/sponsors/PHLAK" class="inline-block rounded-lg bg-pink-600 shadow p-4 mx-2 text-lg text-white sm:px-6 hover:bg-pink-700 hover:shadow-md">
+                <i class="fas fa-heart fa-lg fa-fw"></i>
+                <span class="hidden sm:inline">Become a Sponsor</span>
+                <span class="inline sm:hidden">Sponsor</span>
+            </a>
 
-                <a href="https://www.paypal.me/ChrisKankiewicz" class="inline-block rounded-lg bg-green-600 shadow p-4 m-2 text-lg text-white sm:my-0 sm:px-6 hover:bg-green-700 hover:shadow-md">
-                    <i class="fas fa-donate fa-lg fa-fw "></i>
-                    <span class="hidden sm:inline">Make a Donation</span>
-                    <span class="inline sm:hidden">Donate</span>
-                </a>
-            </div>
-
-            <p class="mt-6 font-serif font-light text-lg">
-                Support development through monthly sponsorship or a one time donation.
-            </p>
+            <a href="https://www.paypal.me/ChrisKankiewicz" class="inline-block rounded-lg bg-green-600 shadow p-4 m-2 text-lg text-white sm:my-0 sm:px-6 hover:bg-green-700 hover:shadow-md">
+                <i class="fas fa-donate fa-lg fa-fw "></i>
+                <span class="hidden sm:inline">Make a Donation</span>
+                <span class="inline sm:hidden">Donate</span>
+            </a>
         </div>
+
+        <p class="mt-6 font-serif font-light text-lg">
+            Support development through monthly sponsorship or a one time donation.
+        </p>
     </div>
 </div>
