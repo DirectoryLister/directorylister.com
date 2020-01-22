22915246993d2f925a25ed6ac42a4bb7274d6b8a
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-22 10:42:54 -0700

    Improved help and support section on smaller screens and added a gradient background

--- resources/views/components/help-and-support.blade.php
+++ resources/views/components/help-and-support.blade.php
@@ -1,8 +1,8 @@
-<div class="bg-blue-700 text-white">
-    <div class="container max-w-4xl mx-auto px-4 py-12 flex justify-center items-center lg:px-0">
-        <i class="fas fa-question-circle text-4xl mr-4"></i>
+<div id="help-and-support" class="bg-blue-700 text-white">
+    <div class="container max-w-4xl mx-auto px-4 py-12 flex flex-col justify-center items-center sm:flex-row lg:px-0">
+        <i class="fas fa-question-circle text-4xl mb-4 sm:mb-0 sm:mr-4"></i>
 
-        <p class="mt font-serif font-light text-lg">
+        <p class="mt font-serif font-light text-lg text-center">
             Join our
             <a href="https://spectrum.chat/directory-lister" class="underline">Community Forum</a>
             for help and support.
