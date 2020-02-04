00a03c8fb3474c2757334493fd5333d254bde3a5
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-02-03 19:50:45 -0700

    Updated buttons

--- resources/views/components/getting-started.blade.php
+++ resources/views/components/getting-started.blade.php
@@ -1,16 +1,18 @@
 <div class="bg-blue-900 text-white">
-    <div class="container max-w-4xl mx-auto px-4 py-12 text-center lg:px-0">
-        @foreach ($release->assets as $asset)
-            <a href="{{ $asset->browser_download_url }}" class="inline-block rounded-lg bg-blue-600 shadow px-6 py-4 text-lg w-full my-2 hover:bg-blue-700 hover:shadow-md sm:w-auto sm:my-0 sm:mx-2">
-                Download
+    <div class="container max-w-4xl mx-auto px-4 py-12 flex flex-col justify-between items-center text-center lg:px-0">
+        <div>
+            @foreach ($release->assets as $asset)
+                <a href="{{ $asset->browser_download_url }}" class="inline-block rounded-lg bg-blue-600 shadow px-6 py-4 text-lg w-full my-2 hover:bg-blue-700 hover:shadow-md sm:w-auto sm:my-0 sm:mx-2">
+                    Download
 
-                <span class="font-bold">
-                    {{ preg_replace(
-                        sprintf('/^DirectoryLister-%s\./', preg_quote($release->tag_name)), '', $asset->name
-                    ) }}
-                </strong>
-            </a>
-        @endforeach
+                    <span class="font-bold">
+                        {{ preg_replace(
+                            sprintf('/^DirectoryLister-%s\./', preg_quote($release->tag_name)), '', $asset->name
+                        ) }}
+                    </strong>
+                </a>
+            @endforeach
+        </div>
 
         <p class="font-serif font-light mt-6">
             <i class="fas fa-info-circle fa-fw"></i>
--- resources/views/components/sponsor.blade.php
+++ resources/views/components/sponsor.blade.php
@@ -1,16 +1,12 @@
 <div class="bg-gray-800 text-white">
     <div class="container max-w-4xl mx-auto py-12 px-4 flex flex-col justify-between items-center text-center lg:px-0">
         <div>
-            <a href="https://github.com/sponsors/PHLAK" class="inline-block rounded-lg bg-pink-600 shadow p-4 mx-2 text-lg text-white sm:px-6 hover:bg-pink-700 hover:shadow-md">
-                <i class="fas fa-heart fa-lg fa-fw"></i>
-                <span class="hidden sm:inline">Become a Sponsor</span>
-                <span class="inline sm:hidden">Sponsor</span>
+            <a href="https://github.com/sponsors/PHLAK" class="inline-block rounded-lg bg-pink-600 shadow my-2 p-4 text-lg text-white w-full hover:bg-pink-700 hover:shadow-md sm:w-auto sm:mx-2 sm:my-0">
+                <i class="fas fa-heart fa-lg fa-fw"></i> Become a Sponsor
             </a>
 
-            <a href="https://www.paypal.me/ChrisKankiewicz" class="inline-block rounded-lg bg-green-600 shadow p-4 m-2 text-lg text-white sm:my-0 sm:px-6 hover:bg-green-700 hover:shadow-md">
-                <i class="fas fa-donate fa-lg fa-fw "></i>
-                <span class="hidden sm:inline">Make a Donation</span>
-                <span class="inline sm:hidden">Donate</span>
+            <a href="https://www.paypal.me/ChrisKankiewicz" class="inline-block rounded-lg bg-green-600 shadow my-2 p-4 text-lg text-white w-full hover:bg-green-700 hover:shadow-md sm:w-auto sm:mx-2 sm:my-0">
+                <i class="fas fa-donate fa-lg fa-fw "></i> Make a Donation
             </a>
         </div>
 
