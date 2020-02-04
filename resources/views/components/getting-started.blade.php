0d913a850cbe1176d93eefece8d8d54a9aef9cf7
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-02-03 19:25:27 -0700

    Updated 'Get started' button to download buttons

--- resources/views/components/getting-started.blade.php
+++ resources/views/components/getting-started.blade.php
@@ -1,10 +1,16 @@
 <div class="bg-blue-900 text-white">
     <div class="container max-w-4xl mx-auto px-4 py-12 text-center lg:px-0">
-        <a href="{{ $release->html_url ?? 'https://github.com/DirectoryLister/DirectoryLister/releases/latest' }}"
-            class="inline-block rounded-lg bg-blue-600 shadow px-6 py-4 text-lg hover:bg-blue-700 hover:shadow-md"
-        >
-            Get Directory Lister
-        </a>
+        @foreach ($release->assets as $asset)
+            <a href="{{ $asset->browser_download_url }}" class="inline-block rounded-lg bg-blue-600 shadow px-6 py-4 text-lg w-full my-2 hover:bg-blue-700 hover:shadow-md sm:w-auto sm:my-0 sm:mx-2">
+                Download
+
+                <span class="font-bold">
+                    {{ preg_replace(
+                        sprintf('/^DirectoryLister-%s\./', preg_quote($release->tag_name)), '', $asset->name
+                    ) }}
+                </strong>
+            </a>
+        @endforeach
 
         <p class="font-serif font-light mt-6">
             <i class="fas fa-info-circle fa-fw"></i>
