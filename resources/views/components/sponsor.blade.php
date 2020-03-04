44c6b2c22b24b0b640d768bf1de35d9359488389
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-03-03 19:54:15 -0700

    Updated persistent donation link to default to $10.00

--- resources/views/components/sponsor.blade.php
+++ resources/views/components/sponsor.blade.php
@@ -5,7 +5,7 @@
                 <i class="fas fa-heart fa-lg fa-fw"></i> Become a Sponsor
             </a>
 
-            <a href="https://www.paypal.me/ChrisKankiewicz" class="inline-block rounded-lg bg-green-600 shadow my-2 px-6 py-4 text-lg text-white w-full hover:bg-green-700 hover:shadow-md sm:w-auto sm:mx-2 sm:my-0">
+            <a href="https://www.paypal.me/ChrisKankiewicz/10.00" class="inline-block rounded-lg bg-green-600 shadow my-2 px-6 py-4 text-lg text-white w-full hover:bg-green-700 hover:shadow-md sm:w-auto sm:mx-2 sm:my-0">
                 <i class="fas fa-donate fa-lg fa-fw "></i> Make a Donation
             </a>
         </div>
