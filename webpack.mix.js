8d6248e8174c357613da6a6fed0281657b24b44c
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-07-15 21:22:41 -0700

    Removed a docblock

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,17 +2,6 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 require('laravel-mix-purgecss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.webpackConfig({
     watchOptions: { ignored: ['node_modules', 'vendor'] }
 });
