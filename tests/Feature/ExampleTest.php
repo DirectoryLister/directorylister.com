3e6b051316aa9a7a9235a5792348c7f060f6897f
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2021-05-10 22:43:26 -0700

    Updatd the defaut test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,11 +6,11 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /** A basic test example. */
-    public function test_basic_test()
+    /** @test */
+    public function it_can_display_the_home_page()
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertViewIs('index');
     }
 }
