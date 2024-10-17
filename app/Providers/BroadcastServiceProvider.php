d07c880c4170d5f6ab630d30ce88ef932217ac1f
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2024-10-17 15:08:54 -0700

    More cleanup

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -8,7 +8,7 @@ use Illuminate\Support\ServiceProvider;
 class BroadcastServiceProvider extends ServiceProvider
 {
     /** Bootstrap any application services. */
-    public function boot()
+    public function boot(): void
     {
         Broadcast::routes();
 
