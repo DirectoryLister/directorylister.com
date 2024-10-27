d46f50b9de3b5d7eb40a0ec86334f1f28636ea21
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2024-10-26 23:14:12 -0700

    Replaced sass with sass-embedded

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                'resources/scss/app.scss',
                 'resources/js/app.js'
             ],
             refresh: true,
