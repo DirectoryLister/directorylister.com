3d448e490a6995254a09043324c099dc7346f65c
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2024-10-26 20:48:41 -0700

    Updated GitHub client

--- app/Http/Clients/GitHub/CachedClient.php
+++ app/Http/Clients/GitHub/CachedClient.php
@@ -1,25 +1,24 @@
 <?php
 
-namespace App\GitHub;
+namespace App\Http\Clients\GitHub;
 
+use App\Http\Clients\GitHub\ClientInterface as GitHubClientInterface;
 use Carbon\CarbonInterval;
 use Illuminate\Support\Facades\Cache;
 use stdClass;
 
-class CachedClient extends Client
+class CachedClient implements GitHubClientInterface
 {
-    /**
-     * Get a cached release.
-     *
-     * @param string $owner The repository owner
-     * @param string $repo The repository name
-     */
+    public function __construct(
+        private GitHubClientInterface $github
+    ) {}
+
     public function latestRelease(string $owner, string $repo): stdClass
     {
         return Cache::remember(
             sprintf('latestRelease:%s:%s', $owner, $repo),
             CarbonInterval::minutes(rand(60, 120)),
-            fn (): stdClass => parent::latestRelease($owner, $repo)
+            fn (): stdClass => $this->github->latestRelease($owner, $repo)
         );
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,29 @@
 
 namespace App\Providers;
 
-use App\GitHub\CachedClient as CachedGitHubClient;
-use App\GitHub\Client as GitHubClient;
+use App\Http\Clients\GitHub;
+use GuzzleHttp\Client as GuzzleHttpClient;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(GitHubClient::class, function ($app) {
-            return new CachedGitHubClient(config('services.github.token'));
+        $this->app->bind(GitHub\ClientInterface::class, function (): GitHub\Client {
+            return new GitHub\Client(
+                new GuzzleHttpClient([
+                    'base_uri' => Config::string('services.github.base_uri'),
+                    'connect_timeout' => 5,
+                    'headers' => [
+                        'Authorization' => sprintf('Token %s', Config::string('services.github.token')),
+                    ],
+                    'timeout' => 30,
+                ])
+            );
         });
+
+        $this->app->extend(GitHub\ClientInterface::class, fn (GitHub\ClientInterface $github): GitHub\ClientInterface => new GitHub\CachedClient($github));
     }
 
     public function boot(): void {}
