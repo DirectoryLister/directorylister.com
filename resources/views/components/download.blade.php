819fd4df2431f003df7423412f96954f225ab39a
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2025-06-21 17:59:05 -0700

    Added content type tracking

--- resources/views/components/download.blade.php
+++ resources/views/components/download.blade.php
@@ -2,7 +2,7 @@
     <div class="container max-w-4xl mx-auto px-4 py-12 flex flex-col justify-between items-center text-center lg:px-0">
         <div>
             @foreach ($release->assets as $asset)
-                <a href="{{ $asset->browser_download_url }}" data-umami-event="download" data-umami-event-version="{{ $release->tag_name }}" data-umami-event-asset="{{ $asset->name }}"
+                <a href="{{ $asset->browser_download_url }}" data-umami-event="download" data-umami-event-version="{{ $release->tag_name }}" data-umami-event-asset="{{ $asset->name }}" data-umami-event-content-type="{{ $asset->content_type }}"
                     class="inline-block rounded-lg bg-sky-600 shadow-sm my-2 px-6 py-4 text-lg text-white w-full transform transition hover:scale-105 hover:shadow-md sm:w-auto sm:mx-2 sm:my-0"
                 >
                     <i class="fas fa-download fa-lg fa-fw"></i> Download
