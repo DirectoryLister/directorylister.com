6d99c66dbd1e49a8666f05498dd519f4a6814219
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2025-08-01 13:38:58 -0600

    Emphasize the screenshot a bit more

--- resources/views/components/intro.blade.php
+++ resources/views/components/intro.blade.php
@@ -1,17 +1,19 @@
 <div class="bg-sky-50">
-    <div class="container max-w-4xl mx-auto px-4 py-12 lg:px-0">
-        <p class="font-serif leading-loose text-lg mb-12 sm:text-xl sm:leading-loose">
+    <div class="container max-w-4xl mx-auto space-y-8 px-4 py-12 lg:px-0">
+        <p class="font-serif leading-loose text-lg sm:text-xl sm:leading-loose">
             <span class="font-bold">Directory Lister</span> is the easiest way
             to expose the contents of any web-accessible folder for browsing
             and sharing. With a zero configuration, drag-and-drop installation
             you'll be up and running in <span class="underline">less than a minute</span>.
         </p>
 
-        <div id="beer-slider" class="beer-slider rounded-lg shadow-lg mb-6" data-beer-label="Dark">
-            <img src="{{ asset('images/screenshot-dark.png') }}" alt="Directory Lister - Dark Theme">
+        <div class="bg-gray-50 border border-gray-200 rounded-lg overflow-hidden p-1.5 lg:-mx-12 xl:-mx-32">
+            <div id="beer-slider" class="beer-slider rounded border border-gray-200 -mb-1.5" data-beer-label="Dark">
+                <img src="{{ asset('images/screenshot-dark.png') }}" alt="Directory Lister - Dark Theme">
 
-            <div class="beer-reveal" data-beer-label="Light">
-                <img src="{{ asset('images/screenshot-light.png') }}" alt="Directory Lister - Light Theme">
+                <div class="beer-reveal" data-beer-label="Light">
+                    <img src="{{ asset('images/screenshot-light.png') }}" alt="Directory Lister - Light Theme">
+                </div>
             </div>
         </div>
 
