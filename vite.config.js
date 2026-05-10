0e1a6391c26da4c41f0c2996001a6896ef23bc77
Chris Kankiewicz <Chris@Kankiewicz.com>
2026-05-09 17:21:56 -0700

    Define hmr.host in vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
     server: {
         host: true,
         port: 5173,
-        strictPort: true
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+        }
     },
     plugins: [
         laravel({
