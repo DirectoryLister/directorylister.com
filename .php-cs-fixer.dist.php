175cf197db83aafba79afa538fd6a6ee533fc165
Chris Kankiewicz <Chris@Kankiewicz.com>
2026-05-09 17:37:00 -0700

    Relocated temporary files to the top-level .cache directory

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,4 +9,6 @@ $finder = PhpCsFixer\Finder::create()->in([
     __DIR__ . DIRECTORY_SEPARATOR . 'tests',
 ]);
 
-return PHLAK\CodingStandards\ConfigFactory::make($finder);
+return PHLAK\CodingStandards\ConfigFactory::make($finder)->setCacheFile(
+    implode(DIRECTORY_SEPARATOR, [__DIR__, '.cache', 'php-cs-fixer.cache'])
+);
